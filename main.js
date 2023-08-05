368222a5ede3cfc14cbf0a96e3a4879dd495e2e2
bytalitha <talithavandervlies@gmail.com>
2023-08-05 15:55:49 +0200

    submenu mobiel

--- main.js
+++ main.js
@@ -13,29 +13,28 @@ menuCheckbox.addEventListener("change", () => {
 });
 
 // Openen en sluiten submenu + Draaien pijltje in submenu
-    const submenuItems = document.querySelectorAll('.navmenu ul li');
-    
-    submenuItems.forEach(item => {
-      const submenuLink = item.querySelector('a');
-      const submenu = item.querySelector('ul');
-      const arrow = submenuLink.querySelector('.arrow');
-      
-      submenuLink.addEventListener('click', function (event) {
-        event.preventDefault();
-        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-    
-        if (submenu.style.display === 'block') {
-            arrow.classList.add('arrow-up'); // Voeg de klasse toe om het pijltje naar boven te draaien
-        } else {
-            arrow.classList.remove('arrow-up'); // Verwijder de klasse om het pijltje terug te draaien naar beneden
-        }
-    
-        submenuItems.forEach(otherItem => {
-            if (otherItem !== item) {
-                otherItem.querySelector('ul').style.display = 'none';
-                otherItem.querySelector('.arrow').classList.remove('arrow-up'); // Zorg ervoor dat andere pijltjes worden gereset naar naar beneden
-            }
-        });
+const submenuItems = document.querySelectorAll('.navmenu ul li');
+
+submenuItems.forEach(item => {
+  const submenuLink = item.querySelector('a');
+  const submenu = item.querySelector('ul');
+  const arrow = submenuLink.querySelector('.arrow');
+
+  submenuLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+
+    if (submenu.style.display === 'block') {
+      arrow.classList.add('arrow-up');
+    } else {
+      arrow.classList.remove('arrow-up');
+    }
+
+    submenuItems.forEach(otherItem => {
+      if (otherItem !== item && otherItem.querySelector('.arrow')) {
+        otherItem.querySelector('ul').style.display = 'none';
+        otherItem.querySelector('.arrow').classList.remove('arrow-up');
+      }
     });
-    
-    });
+  });
+});
