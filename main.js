163a63a07e9d487dc2455634351955ed038740bd
bytalitha <talithavandervlies@gmail.com>
2023-08-14 22:02:40 +0200

    indeling development pagina

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ menuCheckbox.addEventListener("change", () => {
 });
 
 // Openen en sluiten submenu + Draaien pijltje in submenu
-const submenuItems = document.querySelectorAll('.navmenu ul li');
+const submenuItems = document.querySelectorAll('.navmenu ul li.has-sub');
 
 submenuItems.forEach(item => {
   const submenuLink = item.querySelector('a');
@@ -38,3 +38,59 @@ submenuItems.forEach(item => {
     });
   });
 });
+
+// Buttons skills
+function filterSelection(leerdoelen) {
+  var alleLeerdoelen, i;
+  alleLeerdoelen = document.getElementsByClassName("filterDiv");
+  for (i = 0; i < alleLeerdoelen.length; i++) {
+      RemoveClass(alleLeerdoelen[i], "show");
+      if (alleLeerdoelen[i].classList.contains(leerdoelen)) {
+          AddClass(alleLeerdoelen[i], "show");
+      }
+  }
+
+  scrollToTop();
+}
+
+function AddClass(element, name) {
+  var newClasses = name.split(" ");
+  element.classList.add(...newClasses);
+}
+
+function RemoveClass(element, name) {
+  var elementClasses = element.className.split(" ");
+  var newClasses = name.split(" ");
+  for (var i = 0; i < newClasses.length; i++) {
+      while (elementClasses.indexOf(newClasses[i]) > -1) {
+          elementClasses.splice(elementClasses.indexOf(newClasses[i]), 1);
+      }
+  }
+  element.className = elementClasses.join(" ");
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("buttonSkills");
+var firstButton = btns[0]; 
+
+firstButton.classList.add("active");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+      for (var j = 0; j < btns.length; j++) {
+          btns[j].classList.remove("active");
+      }
+
+      this.classList.add("active");
+
+      var filter = this.getAttribute("data-filter");
+      filterSelection(filter);
+  });
+}
+
+btns[0].click();
+
